569c5e93a0183eb984b224f983ade17acb114c08
Rémi Allaire <remiallaire@umontreal.ca>
2025-06-30 22:33:55 -0400

    Test de synchro

---
+++ Semaine1/chebyshev_points.m
@@ -0,0 +1,12 @@
+% chebyshev_points.m
+% Ce script génère et trace les points de Chebyshev de première espèce
+
+N = 16;
+k = 0:N;
+x = cos(pi * k / N); % Points de Chebyshev
+
+plot(x, zeros(size(x)), 'o')
+title('Points de Chebyshev')
+xlabel('x')
+ylim([-0.1 0.1])c
+grid on
